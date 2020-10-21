9857aa9b49414da5b0219e8961d888839ef05022
unknown <610706@cs.mum.edu>
2020-10-20 19:30:46 -0500

    configure kafka

---
+++ src/main/java/com/sezzle/Calculator/model/KafkaConstants.java
@@ -0,0 +1,7 @@
+package com.sezzle.Calculator.model;
+
+public class KafkaConstants {
+    public static final String KAFKA_TOPIC = "kafka_chat";
+    public static final String GROUP_ID = "calculator_group_id";
+    public static final String KAFKA_BROKER = "localhost:9092";
+}
