21b029928dbecdda5cebc2cd5547d6a81fa6d4cc
unknown <610706@cs.mum.edu>
2020-10-20 20:51:58 -0500

    webSocket confinguration

---
+++ src/main/java/com/sezzle/Calculator/service/MessageConsumerService.java
@@ -0,0 +1,24 @@
+package com.sezzle.Calculator.service;
+
+import com.sezzle.Calculator.model.KafkaConstants;
+import com.sezzle.Calculator.model.Message;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MessageConsumerService {
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @KafkaListener(
+            topics = KafkaConstants.KAFKA_TOPIC,
+            groupId = KafkaConstants.GROUP_ID
+    )
+    public void listen(Message message) {
+        System.out.println("sending via kafka listener..");
+        template.convertAndSend("/topic/group", message);
+    }
+}
